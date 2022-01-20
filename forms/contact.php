f5ea791c85c26a614ab2688369379886c9a07931
Richie <alexisbienayme@gmail.com>
2022-01-20 22:15:41 +0100

    add php file

---
+++ forms/contact.php
@@ -0,0 +1,34 @@
+<?php
+
+    $receiving_email_address = 'chibienayme@gmail.com';
+    
+      if( file_exists('../assets/vendor/php-email-form/php-email-form.php')) {
+    include('../assets/vendor/php-email-form/php-email-form.php');
+} else {
+    die( 'Coming Soon!');
+}
+    
+      $contact = new PHP_Email_Form;
+      $contact->ajax = true;
+      
+      $contact->to = $receiving_email_address;
+      $contact->from_name = $_POST['name'];
+      $contact->from_email = $_POST['email'];
+      $contact->subject = $_POST['subject'];
+    
+      // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+      /*
+      $contact->smtp = array(
+        'host' => 'example.com',
+        'username' => 'example',
+        'password' => 'pass',
+        'port' => '587'
+      );
+      */
+    
+      $contact->add_message( $_POST['name'], 'From');
+      $contact->add_message( $_POST['email'], 'Email');
+      $contact->add_message( $_POST['message'], 'Message', 10);
+    
+      echo $contact->send();
+    ?>
